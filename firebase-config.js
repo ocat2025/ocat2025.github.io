cbaacce6d0390e02240770ec2bf528a13829f299
ocat2025 <ocat.owner@gmail.com>
2025-07-17 20:16:16 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,7 +3,7 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-aut
 
 const firebaseConfig = {
   apiKey: "AIzaSyAXMKz2udLxlC_d3uuv9weFZi5Ja36KX5M",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  authDomain: "env-api-password.firebaseapp.com",
   projectId: "env-api-password",
   appId: "1:850492800142:web:47d519af1b4fbde5d85ddf"
 };
